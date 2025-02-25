9a7c624185673e1860c423a3c881e8d8a40ace28
MEGA <MEGA@gmail.com>
2025-02-25 17:45:11 +0200

    zad 2

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = ArrayCreator2.CreateArray(10, false);
+            Console.WriteLine("Масив: " + string.Join(", ", array));
+
+            int key = int.Parse(Console.ReadLine());
+            Console.WriteLine("Kluch: " + key);
+
+            int index = LinearSearchWithMoveToFront(array, key);
+            Console.WriteLine("Sled premestvane nai-otpred: " + string.Join(", ", array));
+            Console.WriteLine("Index: " + index);
+
+        }
+        public static int LinearSearchWithMoveToFront(int[] arr, int key)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == key)
+                {
+                    (arr[i], arr[0]) = (arr[0], arr[i]);
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
--- ConsoleApp45/Program.cs
+++ ConsoleApp45/Program.cs
@@ -10,10 +10,26 @@
             int key = int.Parse(Console.ReadLine()); 
             Console.WriteLine("Kluch: " + key);
 
-            int index1 = LineinoPurvi.LinearSearchWithTransposition(array, key);
+            int index1 = LinearSearchWithTransposition(array, key);
             Console.WriteLine("Sled transortirane: " + string.Join(", ", array));
             Console.WriteLine("Index: " + index1);
             
+        }
+
+        public static int LinearSearchWithTransposition(int[] arr, int key)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == key)
+                {
+                    if (i > 0)
+                    {
+                        (arr[i], arr[i - 1]) = (arr[i - 1], arr[i]);
+                    }
+                    return i - 1;
+                }
+            }
+            return -1;
         }
     }
 }
