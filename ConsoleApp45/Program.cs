7a6884a410099869ea7fd2eb4b589ffaece3e27c
MEGA <MEGA@gmail.com>
2025-02-25 17:39:08 +0200

    purva zadacha

--- ConsoleApp45/Program.cs
+++ ConsoleApp45/Program.cs
@@ -4,6 +4,15 @@
     {
         static void Main(string[] args)
         {
+            int[] array = ArrayCreator.CreateArray(10, false);
+            Console.WriteLine("Masiv: " + string.Join(", ", array));
+
+            int key = int.Parse(Console.ReadLine()); 
+            Console.WriteLine("Kluch: " + key);
+
+            int index1 = LineinoPurvi.LinearSearchWithTransposition(array, key);
+            Console.WriteLine("Sled transortirane: " + string.Join(", ", array));
+            Console.WriteLine("Index: " + index1);
             
         }
     }
